2ac53e15c16afcaa8b9014e357923043bb20aa83
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-13 13:22:34 +0100

    RYMVP add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'La Tour dArgent',
+    address:      '17 Quai de la Tournelle, 75005 Paris',
+    phone_number: '01 43 54 23 31',
+    category:     'french'
+  },
+  {
+    name:         'Aasahi',
+    address:      '2 Rue des Chapeliers, 44000 Nantes',
+    phone_number: '02 40 47 05 68',
+    category:     'japanese'
+  },
+  {
+    name:         'Gino Pizza',
+    address:      '6 Rue des Petites Écuries, 44000 Nantes',
+    phone_number: '02 40 74 11 13',
+    category:     'italian'
+  },
+  {
+    name:         'La Frite',
+    address:      '12 rue de bruxelles, 75003 Paris',
+    phone_number: '01 43 75 25 83',
+    category:     'belgian'
+  },
+  {
+    name:         'Le jardin de manchourie',
+    address:      '34 Allée Vivaldi, 75012 Paris',
+    phone_number: '01 43 45 58 88',
+    category:     'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
