67c64526dc7745cefd1cdcfd20d719d2ef45035d
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-13 14:56:36 +0100

    RYMVP add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :new, :create, :show] do
-    resources :reviews, only: [:index, :new, :create, :show]
+    resources :reviews, only: [:index, :new, :create]
   end
 end
